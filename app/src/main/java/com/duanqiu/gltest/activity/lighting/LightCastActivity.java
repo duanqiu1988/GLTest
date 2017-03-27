a8037d10bda10a4d29f3e23b3f6aa82b5c6f5979
duanqiu1988 <duanqiu1988@hotmail.com>
2017-03-27 17:42:51 +0800

    light cast

---
+++ app/src/main/java/com/duanqiu/gltest/activity/lighting/LightCastActivity.java
@@ -0,0 +1,20 @@
+package com.duanqiu.gltest.activity.lighting;
+
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+
+import com.duanqiu.gltest.BaseCameraActivity;
+import com.duanqiu.gltest.render.lighting.LightCastRenderer;
+
+public class LightCastActivity extends BaseCameraActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected GLSurfaceView.Renderer getRenderer() {
+        return new LightCastRenderer(this);
+    }
+}
